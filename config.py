49f10cf5d341c3d936c500806e808d2fa1175130
minbootal9ive7 <plsgoooglebb@gmail.com>
2026-08-26 21:17:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = "ضع_توكن_البوت_هنا"
+
+BOT_NAME = "Nightfall Games"
+
+STARTING_COINS = 100
+
+COLORS = {
+    "main": 0x5865F2,
+    "success": 0x57F287,
+    "danger": 0xED4245,
+    "warning": 0xFEE75C
+}
